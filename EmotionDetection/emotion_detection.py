37640ed2e5d514713079f08f46f5ed721a94d645
Jonathan Bursztyn <jobur93@gmail.com>
2026-09-24 21:48:56 +0100

    Handle blank input with status code 400

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,6 +8,16 @@ def emotion_detector(text_to_analyze):
     myobj = {"raw_document": {"text": text_to_analyze}}
     response = requests.post(url, json=myobj, headers=headers, timeout=10)
 
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None,
+        }
+
     emotions = json.loads(response.text)['emotionPredictions'][0]['emotion']
     result = {
         'anger': emotions['anger'],
